2be4b3b392aed42a614ec48a4677d73489f1d172
sivakumar-2005 <sivak6168@gmail.com>
2024-09-26 16:11:12 +0530

    Add files via upload

---
+++ Check prime number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int isPrime(int n) {
+    if (n <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1; 
+}
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    if (isPrime(n)) {
+        printf("%d is a prime number.\n", n);
+    } else {
+        printf("%d is not a prime number.\n", n);
+    }
+
+    return 0;
+}
+
